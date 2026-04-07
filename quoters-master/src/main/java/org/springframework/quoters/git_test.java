0c517a80989d783d6cb1c69a9dbdf2013da98e3d
Oooc <15715062991@163.com>
2026-04-07 20:27:10 +0800

    first commit

--- quoters-master/src/main/java/org/springframework/quoters/git_test.java
+++ quoters-master/src/main/java/org/springframework/quoters/git_test.java
@@ -2,8 +2,8 @@ package org.springframework.quoters;
 
 public class git_test {
     public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("hello git");
-        System.out.println("hello world");
+        System.out.println("hello world1");
+        System.out.println("hello git2");
+        System.out.println("hello world3");
     }
 }
