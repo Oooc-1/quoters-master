e4cbc7be587d04a89a52cc85553bac1a2b08dfaa
Oooc-1 <15715062991@163.com>
2026-04-07 20:24:55 +0800

    Update git_test.java

--- quoters-master/src/main/java/org/springframework/quoters/git_test.java
+++ quoters-master/src/main/java/org/springframework/quoters/git_test.java
@@ -2,8 +2,8 @@ package org.springframework.quoters;
 
 public class git_test {
     public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("hello git");
-        System.out.println("hello world");
+        System.out.println("hello world！");
+        System.out.println("hello git！");
+        System.out.println("hello world！");
     }
 }
